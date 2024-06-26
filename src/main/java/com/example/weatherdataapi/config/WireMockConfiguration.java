17bbd39cafe103732cb9d6e2c6ba540b2f2d5d37
mart-gant <martgant@gmail.com>
2024-06-26 18:10:24 +0200

    first version

--- src/main/java/com/example/weatherdataapi/config/WireMockConfiguration.java
+++ src/main/java/com/example/weatherdataapi/config/WireMockConfiguration.java
@@ -1,7 +1,5 @@
 package com.example.weatherdataapi.config;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.core.Options;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/example/weatherdataapi/dto/CityDTO.java
+++ src/main/java/com/example/weatherdataapi/dto/CityDTO.java
@@ -2,7 +2,10 @@ package com.example.weatherdataapi.dto;
 
 public class CityDTO {
 
+    private final Double longitude;
+
     public CityDTO(String name, Double longitude) {
+        this.longitude = longitude;
     }
 }
 
